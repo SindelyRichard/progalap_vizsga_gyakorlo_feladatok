8d3dc47c1e87a70b50a5ca16624c90edf9531c95
Sindely Richárd <113032480+SindelyRichard@users.noreply.github.com>
2023-06-22 15:47:14 +0200

    Update rendezes.c

--- rendezes.c
+++ rendezes.c
@@ -7,13 +7,10 @@ void maximum_kivalasztas_rendezes(int *tomb, int meret);
 void minimum_kivalasztas_rendezes(int *tomb, int meret);
 void beszuro_rendezes(int *tomb,int meret);
 void szamlalo_rendezes(int *tomb,int *index,int meret);
-void beolvas(int *tomb,int meret);
 
 int main()
 {
-
-    int tomb[8];// = {3,8,2,9,1,45,65,99};
-    beolvas(tomb,8);
+    int tomb[8] = {3,8,2,9,1,45,65,99};
     int index[]={1,1,1,1,1,1,1,1};
     int meret = sizeof(tomb)/sizeof(int);
     buborek_rendezes(tomb,meret);
@@ -45,8 +42,6 @@ void buborek_rendezes(int *tomb,int meret)
         }
         
     }
-    
-    
 }
 void kiir(int *tomb,int meret)
 {
@@ -54,7 +49,6 @@ void kiir(int *tomb,int meret)
     {
         printf("%d\n",tomb[i]);
     }
-    
 }
 void maximum_kivalasztas_rendezes(int *tomb, int meret)
 {
@@ -62,8 +56,6 @@ void maximum_kivalasztas_rendezes(int *tomb, int meret)
     for (int i = 0; i < meret; i++)
     {
         maxindex = i;
-        
-        
         for (int j = i+1; j < meret; j++)
         {
             if(tomb[j]>tomb[maxindex])
@@ -75,7 +67,6 @@ void maximum_kivalasztas_rendezes(int *tomb, int meret)
         tomb[i]=tomb[maxindex];
         tomb[maxindex] = temp;
     }
-    
 }
 void minimum_kivalasztas_rendezes(int *tomb, int meret)
 {
@@ -83,8 +74,6 @@ void minimum_kivalasztas_rendezes(int *tomb, int meret)
     for (int i = 0; i < meret; i++)
     {
         minindex = i;
-        
-        
         for (int j = i+1; j < meret; j++)
         {
             if(tomb[j]<tomb[minindex])
@@ -101,22 +90,19 @@ void beszuro_rendezes(int *tomb,int meret)
 {
     int j=1;
     int seged = 0;
-    for (int i=2; i < meret-1; i++)
+    for (int i=2; i < meret; i++)
     {
         seged = tomb[i];
-        for (j = i-1; j>0 && seged<tomb[j]; j--)
+        for (j = i-1; j>=0 && seged<tomb[j]; j--)
         {
             tomb[j+1] = tomb[j];
             
         }
         tomb[j+1] = seged;
-        
-    }
-    
+    }   
 }
 void szamlalo_rendezes(int *tomb,int *index,int meret)
 {
-    
     for (int i = 1; i < meret; i++)
     {
         for (int j = 0; j < meret; j++)
@@ -126,36 +112,7 @@ void szamlalo_rendezes(int *tomb,int *index,int meret)
                index[i] = index[i]+1;
             }
             else index[j] = index[j]+1;
-        }
-        
+        }     
     }
-    
+}
 
-}
-void beolvas(int *tomb,int meret)
-{
-    
-    FILE *fp;
-    char ch;
-    int db = 0;
-    int i = 0;
-    fp = fopen("ertek.txt","r");
-    if(!fp)
-    {
-        printf("\nHiba tortent a file megnyitasa kozben!");
-    }
-    else
-    {
-        while((ch = fgetc(fp)) != EOF) { if(ch == '\n') db++; }
-        if (db==0) { printf("\nFile is empty ...\n"); return ; }
-        rewind(fp);
-    
-        while(fscanf(fp, "%d", &tomb[i])!=EOF)
-        {
-            i++;
-        }
-    
- } 
-    
-    fclose(fp); 
-}
